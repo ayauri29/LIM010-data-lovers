f9d208d4eabe4638adb2bd7d1a4abf40d8f9b326
ayauri29 <albayauri.29@gmail.com>
2019-07-12 11:18:07 -0500

    Subiendo tests

--- src/data.js
+++ src/data.js
@@ -6,7 +6,9 @@
 const example = () => {
   return 'example';
 };
-
+const searchName = (data, name) => {
+  return data.filter(dataRow => (dataRow.name.toLowerCase().indexOf(name.toLowerCase()) > -1) || (dataRow.name.toUpperCase().indexOf(name.toUpperCase()) > -1) || (dataRow.name.indexOf(name) > -1));
+};
 const searchType = (data, tipo) => {
   let array = [];
   array = data.filter(datum => datum.type[0] === tipo || datum.type[1] === tipo);
@@ -18,23 +20,22 @@ const showImg = (array) => {
     if (array[i].multipliers === null) {
       let card = `
       <div class="flip-card">
-      <div class="flip-card-inner">
-        <div class="flip-card-front-no">
-            
-              <p class="num">` + 'N.º ' + array[i].num + `</p>
-              <p class="data-value white"><span class="data-type">Estado:</span>` + 'NO ATRAPADO' + `</p>
-              <p class="data-value white"><span class="data-type">Frec. aparición:</span>` + array[i].spawn_chance + '%' + `</p>
-              <img class="img-pok" src="` + array[i].img + `"/>
-              <p class="nom orange">` + array[i].name + `</p>
-              <p class="data-value white"><span class="data-type">Tipo:</span>` + array[i].type.join(' - ') + `</p>
+        <div class="flip-card-inner">
+          <div class="flip-card-front-no">
+            <p class="num">` + 'N.º ' + array[i].num + `</p>
+            <p class="data-value white"><span class="data-type">Estado:</span>` + 'NO ATRAPADO' + `</p>
+            <p class="data-value white"><span class="data-type">Frec. aparición:</span>` + array[i].spawn_chance + '%' + `</p>
+            <img class="img-pok" src="` + array[i].img + `"/>
+            <p class="nom orange">` + array[i].name + `</p>
+            <p class="data-value white"><span class="data-type">Tipo:</span>` + array[i].type.join(' - ') + `</p>
           </div>
           <div class="flip-card-back">
-              <p class="nom black">` + array[i].name + `</p>          
-              <p class="data-value"><span class="data-type">Estatura:</span>` + array[i].height + `</p>
-              <p class="data-value"><span class="data-type">Peso:</span>` + array[i].weight + `</p>
-              <p class="data-value"><span class="data-type">Huevos:</span>` + array[i].egg + `</p>
-              <p class="data-value"><span class="data-type">Tiempo de aparición:</span>` + array[i].spawn_time + `</p>
-              <p class="data-value"><span class="data-type">Debilidades:</span>` + array[i].weaknesses.join(' - ') + '</p>';
+            <p class="nom black">` + array[i].name + `</p>          
+            <p class="data-value"><span class="data-type">Estatura:</span>` + array[i].height + `</p>
+            <p class="data-value"><span class="data-type">Peso:</span>` + array[i].weight + `</p>
+            <p class="data-value"><span class="data-type">Huevos:</span>` + array[i].egg + `</p>
+            <p class="data-value"><span class="data-type">Tiempo de aparición:</span>` + array[i].spawn_time + `</p>
+            <p class="data-value"><span class="data-type">Debilidades:</span>` + array[i].weaknesses.join(' - ') + '</p>';
 
       if (array[i].hasOwnProperty('prev_evolution')) {
         card += '<p class="data-value"><span class="data-type">Pre-evoluciones:</span>' + array[i].prev_evolution.map(evo => evo.name).join(' - ') + '</p>';
@@ -47,8 +48,6 @@ const showImg = (array) => {
       } else {
         card += '<p class="data-value"><span class="data-type">Evoluciones:</span>' + 'No tiene evoluciones' + '</p>';
       }
-    
-  
       card += ` </div>
         </div>
         </div>
@@ -59,7 +58,6 @@ const showImg = (array) => {
       <div class="flip-card">
       <div class="flip-card-inner">
         <div class="flip-card-front">
-            
               <p class="num">` + 'N.º ' + array[i].num + `</p>
               <p class="data-value blue"><span class="data-type">Estado:</span>` + 'ATRAPADO' + `</p>
               <p class="data-value blue"><span class="data-type">Frec. aparición:</span>` + array[i].spawn_chance + '%' + `</p>
@@ -68,7 +66,6 @@ const showImg = (array) => {
               <p class="data-value blue"><span class="data-type">Tipo:</span>` + array[i].type.join(' - ') + `</p>
           </div>
           <div class="flip-card-back">
-  
               <p class="nom black">` + array[i].name + `</p>   
               <p class="data-value"><span class="data-type">CP:</span>` + array[i].multipliers.join(' - ') + `</p>       
               <p class="data-value"><span class="data-type">Estatura:</span>` + array[i].height + `</p>
@@ -77,25 +74,22 @@ const showImg = (array) => {
               <p class="data-value"><span class="data-type">Huevos:</span>` + array[i].egg + `</p>
               <p class="data-value"><span class="data-type">Tiempo de aparición:</span>` + array[i].spawn_time + `</p>
               <p class="data-value"><span class="data-type">Debilidades:</span>` + array[i].weaknesses.join(' - ') + '</p>';
-      
+
       if (array[i].hasOwnProperty('prev_evolution')) {
         card += '<p class="data-value"><span class="data-type">Pre-evoluciones:</span>' + array[i].prev_evolution.map(evo => evo.name).join(' - ') + '</p>';
       } else {
         card += '<p class="data-value"><span class="data-type">Pre-evoluciones:</span>' + 'No tiene pre evoluciones' + '</p>';
       }
-      
+
       if (array[i].hasOwnProperty('next_evolution')) {
         card += '<p class="data-value"><span class="data-type">Evoluciones:</span>' + array[i].next_evolution.map(evo => evo.name).join(' - ') + '</p>';
       } else {
         card += '<p class="data-value"><span class="data-type">Evoluciones:</span>' + 'No tiene evoluciones' + '</p>';
       }
-            
-  
       card += ` </div>
         </div>
         </div>
             `;
-
       list += card;
     }
   }
@@ -186,6 +180,7 @@ const pushTable = (array, index) => {
   return tabla;
 };
 
+window.searchName = searchName;
 window.searchType = searchType;
 window.showImg = showImg;
 window.divideAtrapped = divideAtrapped;
@@ -194,5 +189,6 @@ window.searchOnlyWeakness = searchOnlyWeakness;
 window.searchEggs = searchEggs;
 window.orderData = orderData;
 window.pushTable = pushTable;
+window.sortId = sortId;
 window.example = example;
 
